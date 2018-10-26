a9c8790c6722d8bb8643be31de3ddcc4120ece03
NipunaCooray <nipuna.cooray@gmail.com>
2018-10-26 19:54:55 +1100

    Media handled

--- app/src/main/java/com/example/nipunac/bakingapp_v1/StepDetailFragment.java
+++ app/src/main/java/com/example/nipunac/bakingapp_v1/StepDetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.nipunac.bakingapp_v1;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.nipunac.bakingapp_v1.model.Step;
@@ -23,6 +25,7 @@ import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.squareup.picasso.Picasso;
 
 public class StepDetailFragment extends Fragment {
 
@@ -34,7 +37,13 @@ public class StepDetailFragment extends Fragment {
 
     TextView mStepDescription;
 
+    boolean playWhenReady = true;
 
+    int currentWindow = 0;
+
+    long playbackPosition = 0;
+
+    TextView emptyMessage;
 
     public StepDetailFragment(){}
 
@@ -50,24 +59,44 @@ public class StepDetailFragment extends Fragment {
         mStepDescription.setText(mStep.getDescription());
 
         playerView = rootView.findViewById(R.id.playerView);
+        emptyMessage = rootView.findViewById(R.id.empty_message);
 
-        String videoUrl = mStep.getVideoURL();
-
-        initializePlayer(Uri.parse(videoUrl));
+        setupViews();
 
         return rootView;
     }
 
+    private void setupViews(){
+        if(mStep.getVideoURL().isEmpty()  && mStep.getThumbnailURL().isEmpty() ){
+            playerView.setVisibility(View.GONE);
+            emptyMessage.setVisibility(View.VISIBLE);
+        }
 
-    public void initializePlayer(Uri uri) {
-        if (mPlayer == null) {
+    }
+
+
+    public void initializePlayer() {
+        if (mPlayer == null && (! mStep.getVideoURL().isEmpty() || ! mStep.getThumbnailURL().isEmpty() ) ){
+
             mPlayer = ExoPlayerFactory.newSimpleInstance(
                     new DefaultRenderersFactory(getActivity()),
                     new DefaultTrackSelector(), new DefaultLoadControl());
             playerView.setPlayer(mPlayer);
-            mPlayer.setPlayWhenReady(true);
-            MediaSource mediaSource = buildMediaSource(uri);
+            mPlayer.setPlayWhenReady(playWhenReady);
+            mPlayer.seekTo(currentWindow, playbackPosition);
+
+            String videoUrl;
+
+            if(! mStep.getVideoURL().isEmpty()){
+                videoUrl = mStep.getVideoURL();
+            }else{
+                videoUrl = mStep.getThumbnailURL();
+            }
+
+            MediaSource mediaSource = buildMediaSource(Uri.parse(videoUrl));
             mPlayer.prepare(mediaSource, true, false);
+
+
         }
     }
 
@@ -77,8 +106,54 @@ public class StepDetailFragment extends Fragment {
                 createMediaSource(uri);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //hideSystemUi();
+        if ((Util.SDK_INT <= 23 || mPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
     private void releasePlayer() {
         if (mPlayer != null) {
+            playbackPosition = mPlayer.getCurrentPosition();
+            playWhenReady = mPlayer.getPlayWhenReady();
             mPlayer.release();
             mPlayer = null;
         }

75f8a1f9854cea7e992851e2281e8a8a4844f008
NipunaCooray <nipuna.cooray@gmail.com>
2018-11-02 22:16:02 +1100

    Unit test for recyclerview working

--- app/src/androidTest/java/com/example/nipunac/bakingapp_v1/BakingAppActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nipunac/bakingapp_v1/BakingAppActivityInstrumentedTest.java
@@ -1,10 +1,12 @@
 package com.example.nipunac.bakingapp_v1;
 
+import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,13 +20,30 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentedTest {
 
-
+    private IdlingResource mIdlingResource;
 
     @Rule public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void registerIdlingResource()
+    {
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        // To prove that the test fails, omit this call:
+        IdlingRegistry.getInstance().register(mIdlingResource);
+    }
 
     @Test
     public void mainActivityBasicTest()
     {
         onView(withId(R.id.RecipeRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+
+    @After
+    public void unregisterIdlingResource()
+    {
+        if (mIdlingResource != null)
+        {
+            IdlingRegistry.getInstance().unregister(mIdlingResource);
+        }
     }
 }
---
+++ app/src/main/java/com/example/nipunac/bakingapp_v1/idlingresource/SimpleIdlingResource.java
@@ -0,0 +1,4 @@
+package com.example.nipunac.bakingapp_v1.idlingresource;
+
+public class SimpleIdlingResource {
+}
